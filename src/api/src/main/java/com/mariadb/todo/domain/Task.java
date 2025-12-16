029560463e5b258908d60d6399a91968b9781fb3
SamuelRobillard <samuelben.robillard@gmail.com>
2025-12-16 12:20:07 -0500

    changement task

--- src/api/src/main/java/com/mariadb/todo/domain/Task.java
+++ src/api/src/main/java/com/mariadb/todo/domain/Task.java
@@ -1,6 +1,11 @@
 package com.mariadb.todo.domain;
 
-import javax.persistence.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
 @Entity
 @Table(name = "tasks")
